2b7404bf344b5e1044f6b89dc7466987597344ca
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-03-14 19:15:41 +0300

    Add IntegerInput component

---
+++ src/components/inputs/IntegerInput.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import BaseNumberInput from './BaseNumberInput.vue'
+import { ref, type PropType, type Ref } from 'vue'
+import type { Validation } from '@/types/Validation'
+
+const integer = defineModel({
+  required: true,
+  type: [Number, null] as PropType<number | null>
+})
+
+const props = defineProps<{
+  placeholder?: string
+  validations?: Validation[]
+}>()
+
+const step: Ref<number> = ref(1)
+</script>
+
+<template>
+  <BaseNumberInput
+    :parser="parseInt"
+    :placeholder="props.placeholder"
+    :step="step"
+    :validations="props.validations"
+    v-model="integer"
+  />
+</template>
+
+<style scoped></style>
